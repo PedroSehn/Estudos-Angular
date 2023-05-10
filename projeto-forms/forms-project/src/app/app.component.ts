dd512924eb2403fc01ff8a4512d3e5e94cc15787
PedroSehn <pedrorsehn@hotmail.com>
2023-05-10 16:42:16 -0300

    adiciona pesquisa automatica de cep no formulário

--- projeto-forms/forms-project/src/app/app.component.ts
+++ projeto-forms/forms-project/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,15 @@ import {NgForm} from '@angular/forms';
 export class AppComponent {
   title = 'forms-project';
 
+  constructor(private http: HttpClient){}
+
   onSubmit(variable: NgForm) {
     console.log(variable)
+  }
+
+  consultCEP(cep:string){
+    let clean_cep = cep.replace(/\D/g, '')
+    this.http.get<any>(`https://viacep.com.br/ws/${clean_cep}/json/`)
+      .subscribe((data) => console.log(data))
   }
 }
--- projeto-forms/forms-project/src/app/app.module.ts
+++ projeto-forms/forms-project/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
@@ -5,11 +6,12 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
